7f7f833011166c6ebac090956ecb6d12098d1832
Kira Tanaka <suein_i@mail.ru>
2016-11-12 00:58:27 +0500

    Create. Task 1.

---
+++ MemoryControl/MemoryControl/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var timer = new Timer();
+
+            using (timer.StartTimer())
+            {
+                // do things
+            }
+            
+            Console.WriteLine(timer.ElapsedMilliseconds);
+        }
+    }
+}
---
+++ MemoryControl/MemoryControl/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace MemoryControl
+{
+    public class Timer: Stopwatch, IDisposable
+    {
+        public Timer StartTimer()
+        {
+            Start();
+            return this;
+        }
+        public void Dispose()
+        {
+            Stop();
+        }
+    }
+}
