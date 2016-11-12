c756ff9155464472b73670707e51ba6d83ab9c4a
Kira Tanaka <suein_i@mail.ru>
2016-11-12 12:32:50 +0500

    Update. Task 2.

---
+++ MemoryControl/BitmapEditor.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryControl
+{
+    public class BitmapEditor : IDisposable
+    {
+        public Bitmap bitmap { get; private set; } = null;
+        public BitmapData bitmapData { get; private set; } = null;
+        public byte[] Pixels { get; set; }
+        IntPtr Iptr = IntPtr.Zero;
+        public int Depth { get; private set; }
+        public int Width { get; private set; }
+        public int Height { get; private set; }
+
+        public BitmapEditor(Bitmap bitmap)
+        {
+            this.bitmap = bitmap;
+        }
+        public void SetPixel(int x,int y, byte r, byte g, byte b)
+        {
+            Width = bitmap.Width;
+            Height = bitmap.Height;
+            int PixelCount = Width * Height;
+            int i = ((y * Width) + x) * Depth / 8;
+            Depth = Bitmap.GetPixelFormatSize(bitmap.PixelFormat);
+            bitmapData = bitmap.LockBits(new Rectangle(0, 0, Width, Height), ImageLockMode.ReadWrite,
+                                         bitmap.PixelFormat);
+            Pixels = new byte[PixelCount * Depth / 8];
+            Iptr = bitmapData.Scan0;
+
+            // Copy data from pointer to array
+            Marshal.Copy(Iptr, Pixels, 0, Pixels.Length);
+
+            Pixels[i] = b;
+            Pixels[i + 1] = g;
+            Pixels[i + 2] = r;
+            
+        }
+        public void Dispose()
+        {
+            Marshal.Copy(Pixels, 0, Iptr, Pixels.Length);
+            bitmap.UnlockBits(bitmapData);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- MemoryControl/Program.cs
+++ MemoryControl/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Drawing.Design;
+using System.IO;
 
 namespace MemoryControl
 {
@@ -12,12 +15,28 @@ namespace MemoryControl
         {
             var timer = new Timer();
 
+            /* using (timer.StartTimer())
+             {
+                 // do things
+             }
+
+             Console.WriteLine(timer.ElapsedMilliseconds);*/
+
+            var bitmap = (Bitmap)Bitmap.FromFile("C:\\image.bmp");
             using (timer.StartTimer())
             {
-                // do things
+                bitmap.SetPixel(0, 1, Color.Red);
             }
-            
             Console.WriteLine(timer.ElapsedMilliseconds);
+            using (timer.StartTimer())
+            {
+                using (var bitmapEditor = new BitmapEditor(bitmap))
+                {
+                    bitmapEditor.SetPixel(0, 1, 255, 255, 255);
+                }
+            }
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            Console.ReadLine();
         }
     }
 }
